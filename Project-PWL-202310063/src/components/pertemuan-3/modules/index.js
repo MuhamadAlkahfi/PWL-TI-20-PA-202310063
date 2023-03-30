4a9a53dafe4b57d9ddb8b7d02eaa8006d72c27b8
Muhamad Alkahfi <92163227+MuhamadAlkahfi@users.noreply.github.com>
2023-03-31 00:07:37 +0700

    pertemuan-3

---
+++ Project-PWL-202310063/src/components/pertemuan-3/modules/index.js
@@ -0,0 +1,11 @@
+import Layout from "../../Tugasdua/Layout";
+import Home from "./components/home/Home";
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render (
+    <React.StrictMode>
+        <Layout>
+            <Home/>
+        </Layout>
+    </React.StrictMode>
+);
